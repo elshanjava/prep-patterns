bea5e046d92a85ef532096b0d86fc4e2fdce07c5
Elshan Mahmudov <elshan.mahmudov@datazen.solutions>
2026-09-04 13:09:52 +0400

    commit

--- src/tdd/zpractice/blockingqueue/BoundedBlockingQueue2.java
+++ src/tdd/zpractice/blockingqueue/BoundedBlockingQueue2.java
@@ -1,69 +1,27 @@
 package tdd.zpractice.blockingqueue;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class BoundedBlockingQueue2<T> {
 
   private Object[] items;
   private int head;
   private int tail;
-  private int count;
-  private final ReentrantLock lock = new ReentrantLock();
-  private final Condition notFull = lock.newCondition();
-  private final Condition notEmpty = lock.newCondition();
 
   public BoundedBlockingQueue2(int capacity) {
-    if (capacity <= 0) {
-      throw new IllegalArgumentException("Capacity should be larger then 0");
-    }
-    this.items = new Object[capacity];
+    items = new Object[capacity];
+
   }
 
-  public void put(T object) throws InterruptedException {
-    lock.lock();
-    try {
-      while (count == items.length) {
-        notFull.await();
-      }
-      enqueue(object);
-    } finally {
-      lock.unlock();
-    }
+  public void put(T a) {
+    items[tail] = a;
+    tail = (tail + 1);
   }
-
 
   @SuppressWarnings("unchecked")
-  public T take() throws InterruptedException {
-    lock.lock();
-    try {
-      while (count==0) {
-        notEmpty.await();
-      }
-      return dequeue();
-    } finally {
-      lock.unlock();
-    }
-  }
-
-  public int size() {
-    return count;
-  }
-
-  private T dequeue() {
+  public T take() {
     T item = (T)items[head];
-    count--;
     items[head] = null;
-    head = (head + 1) % items.length;
-    notFull.signal();
+    head = (head + 1);
     return item;
   }
-
-  private void enqueue(T object) {
-    items[tail] = object;
-    tail = (tail + 1) % items.length;
-    count++;
-    notEmpty.signal();
-  }
-
 }
--- test/tdd/blockingqueue/BoundedBlockingQueue2Test.java
+++ test/tdd/blockingqueue/BoundedBlockingQueue2Test.java
@@ -17,85 +17,21 @@ public class BoundedBlockingQueue2Test {
 
   @BeforeEach
   void setUp() {
-    blockingQueue2 = new BoundedBlockingQueue2<>(3);
+    blockingQueue2 = new BoundedBlockingQueue2<>(5);
   }
 
   @Test
-  void putThenTakeElement() throws InterruptedException {
+  void putThenTakeElement() {
     blockingQueue2.put("a");
-
     assertThat(blockingQueue2.take()).isEqualTo("a");
-    assertThat(blockingQueue2.size()).isZero();
   }
 
   @Test
-  void throwsWhenCapacityIsNotPositive() {
-
-    assertThatThrownBy(()-> {
-      new BoundedBlockingQueue2<>(-1);
-    }).isInstanceOf(IllegalArgumentException.class);
-
-    assertThatThrownBy(()-> {
-      new BoundedBlockingQueue2<>(0);
-    }).isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  void takeReturnsElementsInFifoOrder() throws InterruptedException {
+  void putAndReturnInFifoOrder() {
     blockingQueue2.put("a");
     blockingQueue2.put("b");
-    blockingQueue2.put("c");
 
     assertThat(blockingQueue2.take()).isEqualTo("a");
     assertThat(blockingQueue2.take()).isEqualTo("b");
-    assertThat(blockingQueue2.take()).isEqualTo("c");
-    assertThat(blockingQueue2.size()).isZero();
-  }
-
-  @Test
-  void wrapsAroundRingBuffer() throws InterruptedException {
-    blockingQueue2.put("a");
-    blockingQueue2.put("b");
-    blockingQueue2.put("c");
-
-    assertThat(blockingQueue2.take()).isEqualTo("a");
-    assertThat(blockingQueue2.take()).isEqualTo("b");
-
-    blockingQueue2.put("d");
-    blockingQueue2.put("e");
-
-    assertThat(blockingQueue2.take()).isEqualTo("c");
-    assertThat(blockingQueue2.take()).isEqualTo("d");
-    assertThat(blockingQueue2.take()).isEqualTo("e");
-  }
-
-  @Test
-  void takeBlocksUntilElementAvailable() throws InterruptedException{
-    var result = new AtomicReference<String>();
-    var started = new CountDownLatch(1);
-
-    Thread consumer = new Thread(()-> {
-      started.countDown();
-      try {
-        result.set(blockingQueue2.take());
-      } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-      }
-
-    });
-    consumer.start();
-
-    started.await();;
-    Thread.sleep(1000);
-
-    assertThat(result.get()).isNull();
-    assertThat(consumer.isAlive()).isTrue();
-
-    blockingQueue2.put("last");
-    consumer.join();
-
-    assertThat(consumer.isAlive()).isFalse();
-    assertThat(result.get()).isEqualTo("last");
-
   }
 }
